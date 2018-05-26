b899419eab353f42c85efb4b70a21743d53770c6
Mitchell Szczepanczyk <mitchell@szcz.org>
2018-05-26 15:49:01 -0500

    Adding 2018 appearance on 26 N. Halsted

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -av /Users/mitchells/Desktop/projects/writings/szcz-dot-org/resources/public/ szcz@szcz.org:/home/szcz/sites/szcz.org/html
+rsync -av /Users/mitchells/Desktop/szcz-dot-org/resources/public/ szcz@szcz.org:/home/szcz/sites/szcz.org/html
