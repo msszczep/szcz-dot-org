515c57de7178590b8dd98bd53fb12f6f4cdef56f
Mitchell Szczepanczyk <mitchell@szcz.org>
2019-01-26 11:29:50 -0600

    More home directory cleanup: redirecting subanagram.pl

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,2 @@
 rsync -av /home/mitchells/szcz-dot-org/resources/public/ szcz@szcz.org:/home/szcz/sites/szcz.org/html
-rsync -av /home/mitchells/szcz-dot.org/.htaccess szcz@szcz.org:/home/szcz/sites/szcz.org/html
+rsync -av .htaccess szcz@szcz.org:/home/szcz/sites/szcz.org/html
