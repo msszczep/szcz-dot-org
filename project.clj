792cebddfd8985c5cc413b759ae509aad4ee1d0c
Mitchell Szczepanczyk <mitchell@szcz.org>
2021-07-10 15:56:35 -0500

    Updating more files

--- project.clj
+++ project.clj
@@ -3,13 +3,15 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.7.0"]
-                           [ring/ring-devel "1.4.0"]
-                           [compojure "1.4.0"]
-                           [ring-server "0.4.0"]
-                           [cryogen-markdown "0.1.1"]
-                           [cryogen-core "0.1.41"]]
-            :plugins [[lein-ring "0.8.13"]]
+            :dependencies [[org.clojure/clojure "1.10.1"]
+                           [ring/ring-devel "1.8.2"]
+                           [compojure "1.6.2"]
+                           [ring-server "0.5.0"]
+                           [cryogen-flexmark "0.1.4"]
+                           [cryogen-core "0.4.0"]]
+            :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
-                   :handler cryogen.server/handler})
+                   :handler cryogen.server/handler}
+            :aliases {"serve"      ["run" "-m" "cryogen.server"]
+                      "serve-fast" ["run" "-m" "cryogen.server" "fast"]})
